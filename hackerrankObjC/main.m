d3c12ffe1702b0330b0adeb46a4b6487d14ab86b
Hominda <hominda.m@gmail.com>
2017-11-21 12:58:57 +0530

    2nd

---
+++ hackerrankObjC/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  hackerrankObjC
+//
+//  Created by Epic Lanka on 11/20/17.
+//  Copyright © 2017 homiOne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! ha ha git repo 😡");
+        NSLog(@"Hello, World! ha ha git repo 😡 2 🏹");
+        NSLog(@"Hello, World! ha ha git repo 😡 2 🏹");
+        NSLog(@"Hello, World! ha ha git repo 😡 2 🏹");
+    return 0;
+}
+}
